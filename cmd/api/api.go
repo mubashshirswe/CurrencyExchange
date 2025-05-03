87dc85d6b054add459c17f4392e35a1f19119e1e
mubashshir3767 <abduganiyevmubashshir3767@gmail.com>
2025-05-04 04:17:02 +0500

    modify: some changes

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,67 +1,69 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"time"
+import (
+	"fmt"
+	"net/http"
+	"time"
 
-// 	"github.com/go-chi/chi/v5"
-// 	"github.com/go-chi/chi/v5/middleware"
-// 	"github.com/mubashshir3767/currencyExchange/internal/store"
-// )
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/mubashshir3767/currencyExchange/internal/store"
+)
 
-// type application struct {
-// 	config   config
-// 	dbConfig dbConfig
-// 	store    store.Storage
-// }
+type application struct {
+	config config
+	store  store.Storage
+}
 
-// type config struct {
-// 	addr        string
-// 	db          dbConfig
-// 	redisConfig redisConfig
-// 	env         string
-// }
+type config struct {
+	addr        string
+	db          dbConfig
+	redisConfig redisConfig
+	env         string
+}
 
-// type dbConfig struct {
-// 	addr         string
-// 	maxOpenConns int
-// 	maxIdleConns int
-// 	maxIdleTime  string
-// }
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
+}
 
-// type redisConfig struct {
-// 	addr    string
-// 	pw      string
-// 	db      int
-// 	enabled bool
-// }
+type redisConfig struct {
+	addr    string
+	pw      string
+	db      int
+	enabled bool
+}
 
-// func (app *application) mount() *chi.Mux {
-// 	r := chi.NewRouter()
+func (app *application) mount() *chi.Mux {
+	r := chi.NewRouter()
 
-// 	r.Use(middleware.RequestID)
-// 	r.Use(middleware.RealIP)
-// 	r.Use(middleware.Recoverer)
-// 	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
 
-// 	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.Timeout(60 * time.Second))
 
-// 	r.Route("/v1", func(r chi.Router) {
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			app.writeResponse(w, http.StatusOK, "ok")
+		})
+	})
 
-// 	})
-// 	return r
-// }
+	return r
+}
 
-// func (app *application) run(mux *chi.Mux) error {
-// 	srv := &http.Server{
-// 		Addr:         app.config.addr,
-// 		Handler:      mux,
-// 		WriteTimeout: time.Second * 30,
-// 		ReadTimeout:  time.Second * 10,
-// 		IdleTimeout:  time.Minute,
-// 	}
+func (app *application) run(mux *chi.Mux) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
 
-// 	fmt.Printf("server has been started on %v env %v", app.config.addr, app.config.env)
-// 	return srv.ListenAndServe()
-// }
+	fmt.Printf("server has been started on %v env %v", app.config.addr, app.config.env)
+	return srv.ListenAndServe()
+}
