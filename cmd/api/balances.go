fad87329d2058f724120248539705de10f4116b9
mubashshir3767 <abduganiyevmubashshir3767@gmail.com>
2025-05-24 17:51:05 +0500

    balance updated

--- cmd/api/balances.go
+++ cmd/api/balances.go
@@ -9,6 +9,7 @@ import (
 type CreateBalancePayload struct {
 	ID         int64 `json:"id"`
 	Balance    int64 `json:"balance"`
+	UserId     int64 `json:"user_id"`
 	InOutLay   int64 `json:"in_out_lay"`
 	CurrencyId int64 `json:"currency_id"`
 	OutInLay   int64 `json:"out_in_lay"`
@@ -18,6 +19,7 @@ type CreateBalancePayload struct {
 type UpdateBalancePayload struct {
 	ID         int64 `json:"id"`
 	Balance    int64 `json:"balance"`
+	UserId     int64 `json:"user_id"`
 	InOutLay   int64 `json:"in_out_lay"`
 	OutInLay   int64 `json:"out_in_lay"`
 	CurrencyId int64 `json:"currency_id"`
@@ -35,11 +37,9 @@ func (app *application) CreateBalanceHandler(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	userID, _ := r.Context().Value(UserKey).(int)
-
 	balance := &store.Balance{
 		Balance:    payload.Balance,
-		UserId:     int64(userID),
+		UserId:     payload.UserId,
 		InOutLay:   payload.InOutLay,
 		OutInLay:   payload.OutInLay,
 		CurrencyId: payload.CurrencyId,
@@ -110,10 +110,9 @@ func (app *application) UpdateBalanceHandler(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	userID, _ := r.Context().Value(UserKey).(int)
 	balance := &store.Balance{
 		Balance:  payload.Balance,
-		UserId:   int64(userID),
+		UserId:   payload.UserId,
 		InOutLay: payload.InOutLay,
 		OutInLay: payload.OutInLay,
 	}
